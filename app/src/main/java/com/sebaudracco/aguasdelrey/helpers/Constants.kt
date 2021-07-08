b93d6f5cfd9aeb3dddb3faff137667b09ca47f1d
sebaudracco <sebaudracco@gmail.com>
2021-07-08 12:27:38 -0300

    Initial commit

---
+++ app/src/main/java/com/sebaudracco/aguasdelrey/helpers/Constants.kt
@@ -0,0 +1,9 @@
+package com.sebaudracco.aguasdelrey.helpers
+
+class Constants {
+    companion object {
+
+        const val EXTRA_USER_ID = "EXTRA_USER_ID"
+
+    }
+}
---
+++ app/src/main/java/com/sebaudracco/aguasdelrey/ui/home/HomeActivity.kt
@@ -0,0 +1,88 @@
+package com.sebaudracco.aguasdelrey.ui.home
+
+import android.app.AlertDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.Menu
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.sebaudracco.aguasdelrey.R
+import com.sebaudracco.aguasdelrey.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var dialog: AlertDialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarHome.toolbar)
+
+        binding.appBarHome.fab.setOnClickListener { view ->
+            Snackbar.make(view, "", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_home2)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        showLoadingDialog()
+        Handler(Looper.getMainLooper()).postDelayed({
+            dialog.dismiss()
+        }, 7000)
+
+    }
+
+    private fun showLoadingDialog() {
+        if (!::dialog.isInitialized) {
+
+            val builder = AlertDialog.Builder(this)
+
+            val view = LayoutInflater.from(this).inflate(R.layout.dialog_receiving_data, null)
+            builder.setView(view)
+
+            builder.setCancelable(false)
+
+            dialog = builder.create()
+
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        }
+        dialog.show()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.home, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_home2)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
